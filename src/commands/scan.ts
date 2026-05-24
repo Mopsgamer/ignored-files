c8606d4d9ac95785709cc4b26ea3d624ae0ddd4c
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2026-05-25 01:18:05 +0200

    fix: typo and picker feat: icons

--- src/commands/scan.ts
+++ src/commands/scan.ts
@@ -9,13 +9,27 @@ import { nameFromTarget, relatedTargets, targetFromName, TargetName } from "../t
 export default async function (): Promise<void> {
 	const title = "Scan for ignored files"
 	const related = (await relatedTargets()).map(nameFromTarget)
-	const targetName = await pickValue(title, "Select the target", ["None", ...related])
+	const targetName = await pickValue(title, "Select the target", [
+		{ label: "None", alwaysShow: true },
+		...related.map<vscode.QuickPickItem>((name) => ({
+			label: name,
+			iconPath:
+				name === "VSCE"
+					? new vscode.ThemeIcon("extensions")
+					: name === "Yarn classic"
+						? new vscode.ThemeIcon("view-ignored-yarn")
+						: new vscode.ThemeIcon("view-ignored-" + name.toLowerCase()),
+		})),
+	])
 	if (!targetName) return
 	if (targetName === "None") {
 		await vscode.commands.executeCommand("viewIgnored.scan.clear")
 		return
 	}
-	const invert = await pickValue(title, "Enable invertion?", ["included", "ignored"])
+	const invert = await pickValue(title + ": " + targetName, "Enable inversion?", [
+		"included",
+		"ignored",
+	])
 	if (!invert) return
 	const start = Date.now()
 	await decorationProvider.deinit()
--- src/pickValue.ts
+++ src/pickValue.ts
@@ -3,13 +3,13 @@ import * as vscode from "vscode"
 export default function pickValue(
 	title: string,
 	placeholder: string,
-	items: string[],
+	items: Array<vscode.QuickPickItem | string>,
 ): Promise<string | undefined> {
 	return new Promise((resolve) => {
 		const quickPick = vscode.window.createQuickPick()
 		quickPick.title = title
 		quickPick.placeholder = placeholder
-		quickPick.items = items.map((label) => ({ label }))
+		quickPick.items = items.map((item) => (typeof item === "string" ? { label: item } : item))
 		quickPick.ignoreFocusOut = true
 
 		quickPick.onDidAccept(() => {
